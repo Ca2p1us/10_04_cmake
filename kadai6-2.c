783ab9ef71acd0a912b3c82391f818bae8078385
Taishi Furugo <furu5.tai4.meiwa@gmail.com>
2024-07-11 13:06:26 +0900

    "fix file"

--- kadai6-2.c
+++ kadai6-2.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
-struct xyz{
+struct xyz
+{
     int x;
     long y;
     double z;
-}
+};
 
-void scan_xyz(){
+void scan_xyz(void)
+{
     int x;
     long y;
     double z;
     printf("int型x = ");
-    scanf("%d",x);
+    scanf("%d", x);
     printf("long型 = ");
-    scanf("%ld",y);
+    scanf("%ld", y);
     printf("double型 = ");
-    scanf("%f",z);
+    scanf("%f", z);
 
-    struct xyz a = {x,y,z}
+    struct xyz a = {x, y, z};
 
-    printf("x = %d\n",a.x);
-    printf("y =  %ld\n",a.y);
-    printf("z = %f\n",a.z);
+    printf("x = %d\n", a.x);
+    printf("y =  %ld\n", a.y);
+    printf("z = %f\n", a.z);
 }
 
 int main(void)
