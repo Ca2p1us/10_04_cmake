25b0ca48a81e55fd567818cd2700d4f6a273bbb0
Taishi Furugo <furu5.tai4.meiwa@gmail.com>
2024-07-11 13:02:05 +0900

    "addnfile"

---
+++ kadai6-2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct xyz{
+    int x;
+    long y;
+    double z;
+}
+
+void scan_xyz(){
+    int x;
+    long y;
+    double z;
+    printf("int型x = ");
+    scanf("%d",x);
+    printf("long型 = ");
+    scanf("%ld",y);
+    printf("double型 = ");
+    scanf("%f",z);
+
+    struct xyz a = {x,y,z}
+
+    printf("x = %d\n",a.x);
+    printf("y =  %ld\n",a.y);
+    printf("z = %f\n",a.z);
+}
+
+int main(void)
+{
+    scan_xyz();
+}
